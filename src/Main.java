1a4a9d089c983ba570d417c86313bfcc598aa530
chinosonnadi <devonmillz1991@gmail.com>
2023-08-03 01:02:43 +0100

    the code is complete

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        boolean productAvaliable=true;
-        
+        boolean productAvailable=true;
+
         if (productAvailable) {
-            System.out.print("yes");
+            System.out.println("yes");
         } else {
             System.out.print("no");
         }
-
+        System.out.println("the code is complete");
 
     }
 }
